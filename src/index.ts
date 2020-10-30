7a0f0112b30f601f431cf354559fadd9968f4523
Juan Navas <navascruzf@gmail.com>
2020-10-30 13:00:52 -0400

    testing

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.use("/", (req: any, res: any) => {
     res.json("Hello from digital ocean app platform wit Dockerfile and tsc");
 });
 
+app.use("/testing", (req: any, res: any) => {
+    res.json("Helloasdasdadasd asdd");
+});
+
 app.listen(3000, "0.0.0.0", function () {
     console.log("Web Server listening on port " + 3000);
     console.log("mode: " + process.env.NODE_ENV);
